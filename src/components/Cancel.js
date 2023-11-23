a6b3127a9e039ecfad117fc11ff73a9e91e5a6fd
Shivam Patel <shivampatek0048@gmail.com>
2023-11-23 22:10:58 +0530

    Initial commit

---
+++ src/components/Cancel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cancel = () => {
+  return (
+    <h1> Try Again! </h1>
+  )
+}
+
+export default Cancel
---
+++ src/components/Sucess.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sucess = () => {
+  return (
+    <h1> Payment Sucess</h1>
+  )
+}
+
+export default Sucess
